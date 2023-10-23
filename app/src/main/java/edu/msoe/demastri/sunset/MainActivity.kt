83957a75248fedf41d9636ceebe12e6993fb126a
John DeMastri <demastri@msoe.edu>
2023-10-22 21:53:59 -0500

    Chapter 25 - p281 after listing 25.14 - <last should say "argb evaluator"> - added AnimatorSet and used chained animations to change color to dark after sundown

--- app/src/main/java/edu/msoe/demastri/sunset/MainActivity.kt
+++ app/src/main/java/edu/msoe/demastri/sunset/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.msoe.demastri.sunset
 
+import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
@@ -46,7 +47,15 @@ class MainActivity : AppCompatActivity() {
             .setDuration(3000)
         sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
 
-        heightAnimator.start()
-        sunsetSkyAnimator.start()
+        val nightSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", sunsetSkyColor, nightSkyColor)
+            .setDuration(1500)
+        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(heightAnimator)
+            .with(sunsetSkyAnimator)
+            .before(nightSkyAnimator)
+        animatorSet.start()
     }
 }
