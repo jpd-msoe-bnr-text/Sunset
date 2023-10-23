5f4982990072600752450d3a96e3b78a5df551aa
John DeMastri <demastri@msoe.edu>
2023-10-22 21:32:35 -0500

    Chapter 25 - p279 after listing 25.11 - add color acceleration - but color isn't an int, so they "kaliedoscope" across the sky...

--- app/src/main/java/edu/msoe/demastri/sunset/MainActivity.kt
+++ app/src/main/java/edu/msoe/demastri/sunset/MainActivity.kt
@@ -4,11 +4,23 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AccelerateInterpolator
+import androidx.core.content.ContextCompat
 import edu.msoe.demastri.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,6 +40,11 @@ class MainActivity : AppCompatActivity() {
             .setDuration(3000)
         heightAnimator.interpolator = AccelerateInterpolator()
 
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+
         heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
