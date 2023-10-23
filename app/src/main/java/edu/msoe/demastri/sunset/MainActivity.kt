b3ef5301cf492b68bcb4d6b2735e2c2aa1938fd7
John DeMastri <demastri@msoe.edu>
2023-10-22 21:17:32 -0500

    Beginning of Chapter 25 - Initial project, with themes and app bar set up to look like the text

---
+++ app/src/main/java/edu/msoe/demastri/sunset/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.msoe.demastri.sunset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
